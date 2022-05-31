61c4f49cce5e3bf28861f674043321e6634dbfda
Tri-M <trisham0910@gmail.com>
2022-05-31 21:46:48 +0530

    fruits section under progress

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,6 +4,13 @@ const MainContainer = () => {
   return (
     <div className="w-full h-auto flex flex-col items-center overflow-x-hidden justify-center">
       <HomeContainer />
+      <section className="w-full">
+        <div className='w-full flex items-center justify-between'>
+          <p className='text-2xl font-semibold capitalize text-headingColor relative'>
+            Fresh Fruits
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
