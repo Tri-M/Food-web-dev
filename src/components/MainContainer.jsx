a3247ecb50a8e1b386445607d99ff7f9399d8504
Tri-M <trisham0910@gmail.com>
2022-06-01 06:42:12 +0530

    fruits head and chevron icons done

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { motion } from "framer-motion";
 import HomeContainer from "./HomeContainer";
+import {MdChevronLeft,MdChevronRight} from "react-icons/md";
 const MainContainer = () => {
   return (
     <div className="w-full h-auto flex flex-col items-center overflow-x-hidden justify-center">
       <HomeContainer />
-      <section className="w-full">
+      <section className="w-full m-6">
         <div className='w-full flex items-center justify-between'>
-          <p className='text-2xl font-semibold capitalize text-headingColor relative'>
+          <p className='text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:bottom-0 before:left-0 before:bg-gradient-to-tr from-blue-400 to-blue-600 transition-all ease-in-out duration-100'>
             Fresh Fruits
           </p>
+          <div className="hidden md:flex gap-3 items-center">
+          <motion.div whileTap={{scale:0.75}} className='w-8 h-8 rounded-lg bg-blue-300 hover:bg-blue-500 cursor-pointer transition-all duration-100 ease-in-out hover: shadow-lg flex items-center justify-center'>
+            <MdChevronLeft className='text-lg text-white'/>
+          </motion.div>
+          <motion.div whileTap={{scale:0.75}} className='w-8 h-8 rounded-lg bg-blue-300 hover:bg-blue-500 cursor-pointer transition-all duration-100 ease-in-out hover: shadow-lg flex items-center justify-center'>
+            <MdChevronRight className='text-lg text-white'/>
+          </motion.div>
+          </div>
         </div>
       </section>
     </div>
