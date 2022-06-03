bd448e3350e703011f133f7a074872937a414082
Abishek <abishek20030324@gmail.com>
2022-06-03 07:20:30 +0530

    Nav bar styling

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -39,7 +39,7 @@ const HomeContainer = () => {
 
         <button
           type="button"
-          className="bg-gradient-to-br from-blue-400 to-blue-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+          className="bg-gradient-to-br from-blue-400 to-blue-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
         >
           Order Now
         </button>
--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -40,6 +40,7 @@ const RowContainer = ({ flag, data, scrollValue }) => {
       {data && data.length > 0 ? (
         data.map((item) => (
           <div
+            whileHover={{scale: 1.2}}
             key={item.id}
             className="w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4  my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative"
           >
