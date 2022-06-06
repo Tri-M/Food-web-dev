2bcd50f7c46791a6609754f23cc567fc12216834
Abishek <abishek20030324@gmail.com>
2022-06-06 19:00:39 +0530

    STYLING

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -13,7 +13,7 @@ const MenuContainer = () => {
   return (
     <section className="w-full my-6" id="menu">
       <div className="w-full flex flex-col items-center justify-center">
-        <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-blue-400 to-blue-600 transition-all ease-in-out duration-100 mr-auto">
+        <p className= "text-2xl font-semibold capitalize text-black relative before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-blue-400 to-blue-600 transition-all ease-in-out duration-100 mr-auto">
           Our Hot Dishes
         </p>
 
@@ -22,6 +22,7 @@ const MenuContainer = () => {
             categories.map((category) => (
               <motion.div
                 whileTap={{ scale: 0.75 }}
+                whileHover={{scale: 1.2}}
                 key={category.id}
                 className={`group ${
                   filter === category.urlParamName ? "bg-cartNumBg" : "bg-card"
